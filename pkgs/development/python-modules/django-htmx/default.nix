fdcaea1c967088c2e1edc1a2315e642eab1dc818
NightFish <nightfish@nightfish.co>
2024-09-25 14:02:34 +1200

    feat: add django-htmx

---
+++ pkgs/development/python-modules/django-htmx/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  django,
+  django-js-asset,
+  python,
+}:
+
+buildPythonPackage rec {
+  pname = "django-htmx";
+  powner = "adamchainz"
+  version = "1.19.0";
+  format = "setuptools";
+
+  src = fetchFromGitHub {
+    owner = powner;
+    repo = pname;
+    rev = version;
+    sha256 = "12y3chxhqxk2yxin055f0f45nabj0s8hil12hw0lwzlbax6k9ss6";
+  };
+
+  propagatedBuildInputs = [
+    django
+    django-js-asset
+  ];
+
+  pythonImportsCheck = [ "htmx" ];
+
+  checkPhase = ''
+    runHook preCheck
+    ${python.interpreter} tests/manage.py test
+    runHook postCheck
+  '';
+
+  meta = with lib; {
+    description = "Extensions for using Django with htmx.";
+    homepage = "https://github.com/adamchainz/django-htmx";
+    maintainers = with maintainers; [ BIOS9 ];
+    license = with licenses; [ mit ];
+  };
+}
