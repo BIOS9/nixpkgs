68318801e0bb8efc071084ddd8b33cdbd828d9da
NightFish <nightfish@nightfish.co>
2024-09-25 13:28:52 +1200

    feat: add netbox 4.1.1

--- pkgs/servers/web-apps/netbox/default.nix
+++ pkgs/servers/web-apps/netbox/default.nix
@@ -3,7 +3,7 @@ let
   generic = import ./generic.nix;
 in
 lib.fix (self: {
-  netbox = self.netbox_3_7;
+  netbox = self.netbox_4_1;
 
   netbox_3_6 = callPackage generic {
     version = "3.6.9";
@@ -32,6 +32,21 @@ lib.fix (self: {
       netbox = nixosTests.netbox_3_7;
       inherit (nixosTests) netbox-upgrade;
     };
+
+    maintainers = with lib.maintainers; [ minijackson n0emis raitobezarius ];
+  };
+
+  netbox_4_1 = callPackage generic {
+    version = "4.1.1";
+    hash = "sha256-61pJbMWXNFnvWI0z9yWvsutdCAP4VydeceANNw0nKak=";
+    extraPatches = [
+      # Allow setting the STATIC_ROOT from within the configuration and setting a custom redis URL
+      ./config.patch
+    ];
+    # tests = {
+    #   netbox = nixosTests.netbox_3_7;
+    #   inherit (nixosTests) netbox-upgrade;
+    # };
 
     maintainers = with lib.maintainers; [ minijackson n0emis raitobezarius ];
   };
